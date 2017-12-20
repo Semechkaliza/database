2d6e95a9f32b39562afbe1dcfaca7232c8cdc8f5
Lizaveta Grishkina <grishkinaelizaveta@gmail.com>
2017-12-20 23:55:31 +0300

    DB for university

---
+++ шты.sql
@@ -0,0 +1,3 @@
+insert into USERS value ('admin1','12345','liza','grishkina','ADMIN',0);
+insert into USERS value ('user1','1234','sasha','petrov','USER',1);
+insert into USERS value ('user2','12345','misha','gusev','USER',-1);
